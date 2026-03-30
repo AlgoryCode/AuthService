50179b49f0f99c0d16fd8b65c58f528b8be8dd2f
trkhmrt <trkhamarat@gmail.com>
2026-03-30 07:48:09 +0300

    Fix

--- src/main/java/com/ael/authservice/service/OtpPasswordChangeService.java
+++ src/main/java/com/ael/authservice/service/OtpPasswordChangeService.java
@@ -1,6 +1,7 @@
 package com.ael.authservice.service;
 
 
+import com.ael.authservice.config.rabbitmq.MailQueueMessage;
 import com.ael.authservice.repository.OtpPasswordChangeRepository;
 import com.ael.authservice.util.GenerateOTP;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OtpPasswordChangeService {
-  private final OtpPasswordChangeRepository otpPasswordChangeRepository;
-  private final GenerateOTP generateOTP;
+    private final OtpPasswordChangeRepository otpPasswordChangeRepository;
+    private final GenerateOTP generateOTP;
+    private final MailQueuePublisher mailQueuePublisher;
 
-  public String createOtpToPasswordChange(){
-         return generateOTP.generateOTP();
-  }
+    public String createOtpToPasswordChange() {
+        mailQueuePublisher.publish(MailQueueMessage.builder()
+                .to("trkhamarat@gmail.com")
+                .body(generateOTP.generateOTP())
+                .subject("OTP PASSWORD SENDED!").build());
+
+        return "Yes!";
+    }
 
 }
